8602174933797149f3fae76423703668da88d794
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-06-16 15:15:32 -0300

    Create CustomersController.js

---
+++ src/app/controllers/CustomersController.js
@@ -0,0 +1,23 @@
+class CustomersController {
+  index(req, res) {
+    
+  }
+
+  show(req, res) {
+    
+  }
+
+  create(req, res) {
+    
+  }
+
+  update(req, res) {
+    
+  }
+
+  destroy(req, res) {
+    
+  }
+}
+
+module.exports = new CustomersController()
