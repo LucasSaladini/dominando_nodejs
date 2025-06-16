cca757309ca8f80ab10f42a7834536119c3bb182
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-06-16 15:43:14 -0300

    Update CustomersController.js

--- src/app/controllers/CustomersController.js
+++ src/app/controllers/CustomersController.js
@@ -1,19 +1,17 @@
-class CustomersController {
-  constructor() {
-    this.customers = [
-      {id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br"},
-      {id: 2, name: "Google", site: "http://google.com"},
-      {id: 3, name: "UOL", site: "http://uol.com.br"}
-    ]
-  }
-  
+let customers = [
+  {id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br"},
+  {id: 2, name: "Google", site: "http://google.com"},
+  {id: 3, name: "UOL", site: "http://uol.com.br"}
+]
+
+class CustomersController { 
   index(req, res) {
-    return res.json(this.customers)
+    return res.json(customers)
   }
 
   show(req, res) {
     const id = parseInt(req.params.id)
-    const customer = this.customers.find(item => item.id === id)
+    const customer = customers.find(item => item.id === id)
     const status = customer ? 200 : 404
 
     return res.status(status).json(customer)
@@ -21,7 +19,7 @@ class CustomersController {
 
   create(req, res) {
     const { name, site } = req.body
-    const id = this.customers[this.customers.length - 1].id + 1
+    const id = customers[customers.length - 1].id + 1
 
     const newCustomer = { id, name, site }
     customers.push(newCustomer)
@@ -33,23 +31,23 @@ class CustomersController {
     const id = parseInt(req.params.id)
     const { name, site } = req.body
 
-    const index = this.customers.findIndex(item => item.id === id)
+    const index = customers.findIndex(item => item.id === id)
     const status = index >= 0 ? 200 : 404
 
     if(index >= 0) {
-      this.customers[index] = { id: parseInt(id), name, site }
+      customers[index] = { id: parseInt(id), name, site }
     }
 
-    return res.status(status).json(this.customers[index])
+    return res.status(status).json(customers[index])
   }
 
   destroy(req, res) {
     const id = parseInt(req.params.id)
-    const index = this.customers.findIndex(item => item.id === id)
+    const index = customers.findIndex(item => item.id === id)
     const status = index >= 0 ? 200 : 404
 
     if(index >= 0) {
-      this.customers.splice(index, 1)
+      customers.splice(index, 1)
     }
 
     return res.status(status).json()
