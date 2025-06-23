f89a5714d809ec7a80f6d73d391468e89aa86ccf
Lucas Saladini <lucassaladini@hotmail.com>
2025-06-23 18:25:23 +0000

    Working on Users Controller

---
+++ src/app/controllers/UsersController.js
@@ -0,0 +1,26 @@
+import * as Yup from "yup";
+
+
+class UsersController {
+    async index() {
+
+    }
+
+    async show() {
+
+    }
+
+    async create() {
+
+    }
+
+    async update() {
+
+    }
+
+    async destroy() {
+
+    }
+}
+
+export default new UsersController();
